c30462b831d94d18db42885e4904fd165fce848b
Thomas Hilaire <thilaire@linagora.com>
2015-01-06 10:39:57 +0100

    Ready to bench OBM 2.5.10

--- docker/run.sh
+++ docker/run.sh
@@ -2,10 +2,7 @@
 
 echo "
 # Dont't change names, only IPs
-172.16.24.101   bench-opush1.ci.obm-int.dc1
-172.16.24.134   bench-opush2.ci.obm-int.dc1
-172.16.24.135   bench-opush3.ci.obm-int.dc1
-172.16.24.137   bench-obm-3.ci.obm-int.dc1
+172.16.24.138   bench-obm-25.ci.obm-int.dc1
 " >> /etc/hosts
 
 ansible-playbook -vv -f 5 -i xen-bench-inventory obm.yml
